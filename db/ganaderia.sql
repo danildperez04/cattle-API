ebcbe298b5f38b3159d45bcb9886aea8b8b7c79f
danildperez04 <gamerdan589@gmail.com>
2022-11-10 08:27:59 -0600

    add(procedure):  Function normalizeText and changes in  procedures

--- db/ganaderia.sql
+++ db/ganaderia.sql
@@ -19,20 +19,21 @@ CREATE TABLE breed(
 CREATE TABLE cow(
     id_cow INT(12) NOT NULL AUTO_INCREMENT,
     cow_name VARCHAR(30),
-    cow_desc TEXT DEFAULT 'LECHERA',
-    alive BOOLEAN,
-    heat BOOLEAN,
-    gender VARCHAR(10) DEFAULT 'MACHO',
+    cow_desc VARCHAR(10) NOT NULL DEFAULT 'LECHERA',
+    alive BOOLEAN DEFAULT 1,
+    heat BOOLEAN DEFAULT 0,
+    gender VARCHAR(10) NOT NULL DEFAULT 'MACHO',
     CHECK (
         gender = 'MACHO'
         OR gender = 'HEMBRA'
     ),
-    cow_shoes BOOLEAN DEFAULT FALSE,
-    dehorned BOOLEAN DEFAULT FALSE,
-    periodic_weight DOUBLE,
-    periodic_height DOUBLE,
+    cow_shoes BOOLEAN NOT NULL DEFAULT FALSE,
+    dehorned BOOLEAN NOT NULL DEFAULT FALSE,
+    periodic_weight DOUBLE NOT NULL,
+    periodic_height DOUBLE NOT NULL,
     clinic_history TEXT,
     medic_history TEXT,
+    birth_date DATE NOT NULL,
     PRIMARY KEY (id_cow)
 );
 CREATE TABLE breedcow(
@@ -46,25 +47,19 @@ CREATE TABLE breedcow(
 CREATE TABLE cowbirth(
     id_cowbirth INT(12) NOT NULL,
     id_cow INT(12),
-    id_father INT(12),
-    id_mother INT(12),
-    -- ESTIMADO DE EMBARAZO DE VACAS ES 9 MESES Y 10 DIAS
     monta_date DATE NOT NULL,
-    birth_date DATE,
-    -- abarca rambién la fecha de parida
+    birth_date DATE NOT NULL,
     weight DOUBLE(4, 4) NOT NULL,
     PRIMARY KEY (id_cowbirth),
-    FOREIGN KEY (id_cow) REFERENCES cow(id_cow),
-    FOREIGN KEY (id_father) REFERENCES cow(id_cow),
-    FOREIGN KEY (id_mother) REFERENCES cow(id_cow)
+    FOREIGN KEY (id_cow) REFERENCES cow(id_cow)
 );
 CREATE TABLE deadcow(
-id_death INT NOT NULL AUTO_INCREMENT,
-id_cow INT,
-death_cause TEXT,
-death_date DATE NOT NULL,
-PRIMARY KEY (id_death),
-FOREIGN KEY (id_cow) REFERENCES cow (id_cow)
+    id_death INT NOT NULL AUTO_INCREMENT,
+    id_cow INT,
+    death_cause TEXT,
+    death_date DATE NOT NULL,
+    PRIMARY KEY (id_death),
+    FOREIGN KEY (id_cow) REFERENCES cow (id_cow)
 );
 -- Vaccines 
 CREATE TABLE vaccine(
@@ -76,7 +71,7 @@ CREATE TABLE vaccine(
 CREATE TABLE inventory(
     id INT(12) NOT NULL AUTO_INCREMENT,
     id_vaccine INT(12),
-    amount INT(8) DEFAULT 0,
+    ml DOUBLE DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY(id_vaccine) REFERENCES vaccine(id_vaccine)
 );
@@ -85,7 +80,7 @@ CREATE TABLE cowvaccine(
     id_cow INT(12) NOT NULL,
     id_vaccine INT(12) NOT NULL,
     vaccine_date DATE NOT NULL,
-    grams DOUBLE(4, 4) NOT NULL,
+    ml DOUBLE(4, 4) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_cow) REFERENCES cow(id_cow),
     FOREIGN KEY (id_vaccine) REFERENCES vaccine(id_vaccine)
@@ -93,14 +88,14 @@ CREATE TABLE cowvaccine(
 -- Operations
 CREATE TABLE thirdpeople(
     id_third INT(12) NOT NULL AUTO_INCREMENT,
-    fullname VARCHAR(16),
+    fullname VARCHAR(16) NOT NULL,
     contact VARCHAR(20),
     PRIMARY KEY (id_third)
 );
 CREATE TABLE operation(
     id_operation INT(12) NOT NULL AUTO_INCREMENT,
-    id_cow INT(12),
-    id_third INT(12),
+    id_cow INT(12) NOT NULL,
+    id_third INT(12) NOT NULL,
     operation VARCHAR(12) DEFAULT 'PURCHASE',
     CHECK(
         operation = 'PURCHASE'
@@ -113,14 +108,14 @@ CREATE TABLE operation(
 -- Lots
 CREATE TABLE lot(
     id_lot INT(12) NOT NULL AUTO_INCREMENT,
-    lot_address VARCHAR(80),
+    lot_address VARCHAR(80) NOT NULL,
     cow_amount INT(8) DEFAULT 0,
     PRIMARY KEY (id_lot)
 );
 CREATE TABLE lotcow(
     id INT(12) NOT NULL AUTO_INCREMENT,
-    id_lot INT(12),
-    id_cow INT(12),
+    id_lot INT(12) NOT NULL,
+    id_cow INT(12) NOT NULL UNIQUE,
     PRIMARY KEY(id),
     FOREIGN KEY (id_lot) REFERENCES lot(id_lot),
     FOREIGN KEY (id_cow) REFERENCES cow(id_cow)
