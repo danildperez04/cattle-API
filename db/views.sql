492a6bdd75e50aaf716291195054e0dff89b8e7a
danildperez04 <gamerdan589@gmail.com>
2022-11-17 12:16:22 -0600

    add(breedcow): GET, CREATE, UPDATE PROCEDURES have been created

--- db/views.sql
+++ db/views.sql
@@ -10,16 +10,16 @@ WHERE alive = 1;
 
 -- BREED
 CREATE VIEW breedcow_view AS
-SELECT bc.id, c.cow_name, b.breed_name, c.periodic_weight, c.periodic_height 
+SELECT bc.id, c.cow_name, b.breed_name
 FROM cow c, breedcow bc, breed b
 WHERE bc.id_cow = c.id_cow  AND bc.id_breed = b.id_breed;
 
--- CREATE VIEW [name] AS
-SELECT c.id_cow, c.cow_name, c.periodic_weight, c.periodic_height, b.id_breed
+CREATE VIEW cow_left_view AS
+SELECT c.id_cow, c.cow_name, b.id_breed
 FROM cow c
 LEFT JOIN breedcow b ON c.id_cow = b.id_cow;
 
--- CREATE VIEW [name] AS
+CREATE VIEW breed_left_view AS
 SELECT b.id_breed, b.breed_name, bc.id_cow
 FROM breed b
 LEFT JOIN breedcow bc ON b.id_breed = bc.id_breed;
