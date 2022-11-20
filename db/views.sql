ba58705625c34aa1056cd396520a3766607b5d34
danildperez04 <gamerdan589@gmail.com>
2022-11-20 11:02:35 -0600

    add(procedure): Merge Juan's Procedures

--- db/views.sql
+++ db/views.sql
@@ -4,6 +4,12 @@ CREATE VIEW cow_view AS
 SELECT * 
 FROM cow;
 
+DROP VIEW IF EXISTS not_alive_cow;
+CREATE VIEW not_alive_cow AS
+SELECT *
+FROM cow
+WHERE alive = 0;
+
 DROP VIEW IF EXISTS alive_cow;
 CREATE VIEW alive_cow AS
 SELECT *
@@ -14,9 +20,16 @@ DROP VIEW IF EXISTS male_cow;
 CREATE VIEW male_cow AS
 SELECT *
 FROM cow
-WHERE gender = 'MALE';
+WHERE gender = 'MACHO';
 
--- BREED
+DROP VIEW IF EXISTS female_cow;
+CREATE VIEW female_cow AS
+SELECT *
+FROM cow
+WHERE gender = 'HEMBRA';
+
+-- BREED -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 DROP VIEW IF EXISTS breed_view;
 CREATE VIEW breed_view AS
 SELECT * 
@@ -24,6 +37,12 @@ FROM breed ORDER BY id_breed;
 
 DROP VIEW IF EXISTS breedcow_view;
 CREATE VIEW breedcow_view AS
+SELECT bc.id, b.breed_name
+FROM cow c, breedcow bc, breed b
+WHERE bc.id_cow = c.id_cow  AND bc.id_breed = b.id_breed;
+
+DROP VIEW IF EXISTS descrptive_breedcow_view;
+CREATE VIEW descrptive_breedcow_view AS
 SELECT bc.id, c.cow_name, b.breed_name
 FROM cow c, breedcow bc, breed b
 WHERE bc.id_cow = c.id_cow  AND bc.id_breed = b.id_breed ORDER BY c.cow_name;
@@ -40,28 +59,49 @@ SELECT b.id_breed, b.breed_name, bc.id_cow
 FROM breed b
 LEFT JOIN breedcow bc ON b.id_breed = bc.id_breed;
 
--- OPERATION 
+-- OPERATION -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 DROP VIEW IF EXISTS operation_view;
 CREATE VIEW operation_view AS
 SELECT b.id_operation, c.fullname, b.operation, a.cow_name  
 FROM cow a, operation b, thirdpeople c
 WHERE b.id_cow = a.id_cow AND b.id_third = c.id_third;
 
--- VACCINE
+-- VACCINE ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 DROP VIEW IF EXISTS inventory_view;
 CREATE VIEW inventory_view AS
 SELECT i.id, v.id_vaccine, v.vaccine_name, v.vaccine_desc, i.ml
 FROM inventory i 
 INNER JOIN vaccine v ON i.id_vaccine = v.id_vaccine;
 
--- LOT
+DROP VIEW IF EXISTS vaccine_cow_view;
+CREATE VIEW vaccine_cow_view AS
+SELECT v.id_vaccine, v.vaccine_name, c.cow_name from vaccine v 
+INNER JOIN 	cow c ON c.id_cow = c.id_cow;
+
+-- LOT -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 DROP VIEW IF EXISTS lotcow_view;
 CREATE VIEW lotcow_view AS 
+SELECT lc.id_cow, lc.id_lot, c.cow_name, c.cow_desc, l.lot_address
+FROM cow c, lotcow lc, lot l
+WHERE lc.id_lot = l.id_lot AND lc.id_cow = c.id_cow;
+
+DROP VIEW IF EXISTS descriptive_lotcow_view;
+CREATE VIEW descriptive_lotcow_view AS 
 SELECT lc.id, l.id_lot, c.cow_name, l.lot_address 
 FROM cow c, lotcow lc, lot l
 WHERE lc.id_lot = l.id_lot AND lc.id_cow = c.id_cow;
 
--- DEADCOW
+DROP VIEW IF EXISTS breed_lotcow_view;
+CREATE VIEW breed_lotcow_view AS 
+SELECT lc.id_cow,lc.id_lot, lc.cow_name, lc.cow_desc, bc.breed_name, lc.lot_address
+FROM lotcow_view lc
+INNER JOIN breedcow_view bc ON lc.id_cow = bc.id_cow;
+
+-- DEADCOW ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 DROP VIEW IF EXISTS deadcow_view;
 CREATE VIEW deadcow_view AS
 SELECT *
